d2fdace5286687bca8a20e071d8884642a738532
Radosław Tomaszewski <r.tomaszewski@gmail.com>
2020-01-29 17:08:42 +0100

    Add first files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Mp4Player from '../src/components/Mp4Player';
+import Movies from '../src/components/Movies';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    movies: [
+      {
+        id: 0,
+        title: "Film jakiśtam 1",
+        url: "https://youtu.be/rLHvyud1zYY",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      },
+      {
+        id: 1,
+        title: "Film jakiśtam 2",
+        url: "https://youtu.be/zvcTNfBY8As",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      },
+      {
+        id: 2,
+        title: "Film jakiśtam 3",
+        url: "https://youtu.be/Y2ATJB9QMkU",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      }
+    ],
+    currentMovie: 0
+  }
+
+  render() {
+    let cur = null
+    for (let i = 0; i < this.state.movies.length; i++) {
+      if (this.state.currentMovie === this.state.movies[i].id) {
+        cur = this.state.movies[i]
+      }
+    }
+    return (
+      <div className="App">
+        <Mp4Player movie={cur} />
+        <Movies movies={this.state.movies} />
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
---
+++ src/components/Mp4Player.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Mp4Player extends React.Component {
+    render() {
+        return (
+            <div className="Mp4Player">
+
+            </div>
+        )
+    }
+}
+
+export default Mp4Player;
